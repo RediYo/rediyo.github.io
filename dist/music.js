b5a5432a494469617ecde0ede66e975d5a9097b4
rediyo <rediyo@qq.com>
2022-02-27 15:43:51 +0800

    Site updated: 2022-02-27 15:43:49

--- dist/music.js
+++ dist/music.js
@@ -3,17 +3,23 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
+{
+        name: '哪里都是你',
+        artist: '队长',
+        url: 'http://www.ytmp3.cn/down/76138.mp3',
+        cover: 'http://p1.music.126.net/lnOnBbP_H-052Hv5ls-QjA==/109951162964628408.jpg?param=130y130',
+      },
+{
+        name: "致你",
+        artist: 'yihuik苡慧',
+        url: 'https://link.jscdn.cn/sharepoint/aHR0cHM6Ly8xZHJpdi1teS5zaGFyZXBvaW50LmNvbS86dTovZy9wZXJzb25hbC9zdG9yXzFkcml2X29ubWljcm9zb2Z0X2NvbS9FUVozS0VleVNDMU12VnFFVTk1Vnp0WUJlUkZGV0M4OHU5dEF3dDNaRmoyS1Zn.mp3',
+        cover: 'https://p1.music.126.net/VDmN2dNpIFu4gTv4bZe6KQ==/109951166254691365.jpg?param=130y130',
+      },
 	{
         name: "平凡之路",
         artist: '朴树',
         url: 'http://www.ytmp3.cn/down/59211.mp3',
         cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
-      },
-	  {
-        name: '哪里都是你',
-        artist: '队长',
-        url: 'https://link.jscdn.cn/sharepoint/aHR0cHM6Ly8xZHJpdi1teS5zaGFyZXBvaW50LmNvbS86dTovZy9wZXJzb25hbC9zdG9yXzFkcml2X29ubWljcm9zb2Z0X2NvbS9FWWRMZmVkbzhROU50NkxFak9zNHZDQUJSZENPbVBDY09NOVpUdU93ZWpGaDZ3.mp3',
-        cover: 'http://p1.music.126.net/lnOnBbP_H-052Hv5ls-QjA==/109951162964628408.jpg?param=130y130',
       },
       {
         name: '你的酒馆对我打了烊',
